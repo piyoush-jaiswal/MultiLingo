b6d8efac12ae2f2ae15a797a82e339104aa9b8ad
Piyoush Jaiswal <the.silent.coder0506@gmail.com>
2024-04-05 17:33:34 +0530

    commit

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -13,11 +13,11 @@ const Translate = () => {
     selectTag.forEach((tag, id) => {
       for (let country_code in countries) {
         let selected =
-          id == 0
-            ? country_code == "en-GB"
+          id === 0
+            ? country_code === "en-GB"
               ? "selected"
               : ""
-            : country_code == "hi-IN"
+            : country_code === "hi-IN"
             ? "selected"
             : "";
         let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
@@ -68,14 +68,14 @@ const Translate = () => {
       icon.addEventListener("click", ({ target }) => {
         if (!fromText.value || !toText.value) return;
         if (target.classList.contains("fa-copy")) {
-          if (target.id == "from") {
+          if (target.id === "from") {
             navigator.clipboard.writeText(fromText.value);
           } else {
             navigator.clipboard.writeText(toText.value);
           }
         } else {
           let utterance;
-          if (target.id == "from") {
+          if (target.id === "from") {
             utterance = new SpeechSynthesisUtterance(fromText.value);
             utterance.lang = selectTag[0].value;
           } else {
